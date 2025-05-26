27f23e0b04250bf34470d872017390eb13071e09
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-05-25 20:27:52 -0700

    toasting

---
+++ src/routes/api/transactions/category/+server.ts
@@ -0,0 +1,16 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { db } from '$lib/server/db';
+import { expenses, expenseUpdateSchema } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { id, category } = await request.json();
+
+	const toUpdate = { categoryId: category.id };
+	const parsed = expenseUpdateSchema.parse(toUpdate);
+
+	await db.update(expenses).set(parsed).where(eq(expenses.id, id));
+
+	return json({ success: true });
+};
