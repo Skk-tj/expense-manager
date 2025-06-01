192b37950f79c4a4cfd8c8ecc423f99178857465
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-06-01 00:45:29 -0700

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			config: 'wrangler.jsonc'
+		})
+	}
 };
 
 export default config;
