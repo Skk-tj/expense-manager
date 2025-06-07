d33706e575fc2d4ec296ec5b1c8076eea5f1e918
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-06-07 14:19:20 -0700

    Update

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,4 +2,9 @@ import { drizzle } from 'drizzle-orm/d1';
 import * as schema from './schema';
 import type { D1Database } from '@cloudflare/workers-types';
 
-export const db = (database: D1Database) => drizzle(database, { schema, logger: true });
+export const db = (database: D1Database | undefined) => {
+	if (!database) {
+		throw new Error('Database is not defined');
+	}
+	return drizzle(database, { schema, logger: true });
+};
