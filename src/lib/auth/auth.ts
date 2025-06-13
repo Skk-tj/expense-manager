6f9f83ffe6a389a5774ccac47538936c28ddb063
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-06-13 01:24:49 -0700

    env var?

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,10 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
+import { SvelteKitAuth, type SvelteKitAuthConfig } from '@auth/sveltekit';
 import { credentials } from '$lib/auth/credentials';
 
-export const { handle } = SvelteKitAuth({
-	providers: [credentials]
+export const { handle } = SvelteKitAuth(async (event) => {
+	const password = event.platform?.env.PASSWORD;
+
+	return {
+		providers: [credentials(password)]
+	} satisfies SvelteKitAuthConfig;
 });
