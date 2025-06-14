33208f7ffa36b4841f8a76b8f505f369dc513a13
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-06-13 21:43:43 -0700

    update

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,14 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.auth();
+export const load: LayoutServerLoad = async (event) => {
+	const session = await event.locals.auth();
+	const currentPath = event.url.pathname;
+	if (currentPath === '/auth/login') {
+		return {
+			session
+		};
+	}
 
 	if (session?.user === null || session?.user === undefined) {
 		redirect(303, '/auth/login');
