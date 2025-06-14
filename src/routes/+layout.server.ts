7e0cd0b88c4a90b85f3b2fe1c982f97f23998de8
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2025-06-13 21:53:13 -0700

    wrong url

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,14 +4,14 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.auth();
 	const currentPath = event.url.pathname;
-	if (currentPath === '/auth/login') {
+	if (currentPath === '/auth/signin') {
 		return {
 			session
 		};
 	}
 
 	if (session?.user === null || session?.user === undefined) {
-		redirect(303, '/auth/login');
+		redirect(303, '/auth/signin');
 	}
 
 	return {
