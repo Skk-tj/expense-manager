d3ab92cdb003eea1e7f9a37b0619cba9bf5ca907
Skk-tj <11616436+Skk-tj@users.noreply.github.com>
2026-01-01 20:43:21 -0800

    feat(luxon): add luxon and its types for improved date handling in schemas

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -10,9 +10,12 @@ export const load: PageServerLoad = async ({ platform }) => {
 	const thisMonth = new Date().getMonth() + 1;
 	const thisYear = new Date().getFullYear();
 
+	const lastMonth = thisMonth === 1 ? 12 : thisMonth - 1;
+	const lastYear = thisMonth === 1 ? thisYear - 1 : thisYear;
+
 	return {
 		sum: await getSpendingOnMonthAndYear(platform?.env.DB, thisMonth, thisYear),
-		sumLastMonth: await getSpendingOnMonthAndYear(platform?.env.DB, thisMonth - 1, thisYear),
+		sumLastMonth: await getSpendingOnMonthAndYear(platform?.env.DB, lastMonth, lastYear),
 		sumByCategories: await getTotalForCategories(platform?.env.DB, thisMonth, thisYear),
 		sumTrend: await getMonthlyTrend(platform?.env.DB),
 		sumTrendByCategory: await getMonthlyTrendByCategory(platform?.env.DB)
